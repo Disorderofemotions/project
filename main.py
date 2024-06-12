aa3e440756fc27409fa37c6689e9a036f175ed40
Disorderofemotions <147741057+Disorderofemotions@users.noreply.github.com>
2024-06-13 01:30:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import camelot
 import pandas as pd
+from sqlalchemy import create_engine
 
 # Путь к PDF файлу
 file_path = "b4.pdf"
@@ -9,27 +10,28 @@ tables = camelot.read_pdf(file_path, pages="all")
 
 df_list = []
 for table in tables:
- df = table.df
-if len(df.columns) > 5:
- df = df[[0, 1, 2, 3, 4, 5]]
- # Переименование столбцов
-df.columns = ['№','Адрес', 'Период', 'Начислено', 'Оплачено', 'Агентское вознаграждение']
+    df = table.df
+    if len(df.columns) > 5:
+        df = df[[0, 1, 2, 3, 4, 5]]
+        df.columns = ['№','Адрес', 'Период', 'Начислено', 'Оплачено', 'Суммаудержанноговознаграждения']
+        
+        df = df.dropna()
+        df = df.drop(index=df.index[0], axis=0)
+        df = df.drop(index=df.index[-1], axis=0)
+        df = df.drop(columns=df.columns[0], axis=1)
+        df = df.drop(columns=df.columns[1], axis=1)
+        # Добавление обработанных столбцов
+        df["Суммаудержанноговознаграждения"] = df["Суммаудержанноговознаграждения"].str.replace(' ', '')
+        df["Начислено"] = df["Начислено"].str.replace(' ', '')
+        df["Процент_вознаграждения"] = (df["Суммаудержанноговознаграждения"].str.replace(',', '.').astype(float) * 100 / df["Начислено"].str.replace(',', '.').astype(float)).round(decimals=1)
 
-# Удаление строк с пустыми значениями
-df = df.dropna()
-#  # Извлечение числовых значений из столбцов "Начислено" и "Агентское вознаграждение"
-# df['Начислено'] = df['Начислено'].str.extract('(\d+.\d+)')
-# df['Агентское вознаграждение'] = df['Агентское вознаграждение'].str.extract('(\d+.\d+)')
+        # Добавление обработанной таблицы в список
+        df_list.append(df)
 
-# # Преобразование значений в числовой формат
-# df['Начислено'] = pd.to_numeric(df['Начислено'])
-# df['Агентское вознаграждение'] = pd.to_numeric(df['Агентское вознаграждение'])
-
-# Расчет процентного вознаграждения
-# df['Процент вознаграждения'] = (df['Агентское вознаграждение'] / df['Начислено']) * 100
-
- # Добавление обработанной таблицы в список
-df_list.append(df)
 final_df = pd.concat(df_list, ignore_index=True)
 
-final_df.to_excel("output72132.xlsx", index=False)
+# Соединение с базой данных SQLite
+engine = create_engine('sqlite:///output.db', echo=False)
+
+# Сохранение данных в базу данных
+final_df.to_sql('output123', con=engine, if_exists='replace', index=False)
